20d1391be34b410c14fd7e27ee46d57a40723705
otis wilcox <otiswilcox@gmail.com>
2021-08-30 23:28:12 -0400

    added basic skellington components for now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+import HomePage from "./components/HomePage";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <HomePage />
+      </Router>
+      <footer className="footer">
+        <span className="text-muted">
+          <a className="apiLink" href="https://otis.codes">
+            otis
+          </a>{" "}
+          |{" "}
+          <a className="apiLink" href="https://otis.codes">
+            codes
+          </a>{" "}
+          <a href="https://www.linkedin.com/in/otiswilcox/">
+            <img
+              className="linkedinlogo"
+              alt="linkedin logo"
+              src="../images/linkedin-logo.png"
+              width="40px"
+            ></img>
+          </a>{" "}
+          |{" "}
+          <a href="https://github.com/gtfotis">
+            <img
+              className="githublogo"
+              alt="github logo"
+              src="../images/github-logo.png"
+              width="40px"
+            ></img>
+          </a>
+        </span>
+      </footer>
     </div>
   );
 }
